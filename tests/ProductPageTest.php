cca5042599ba05f1e297270648ec9f95adf69d24
Bruno Meilick <b@bnomei.com>
2025-03-30 19:59:12 +0100

    :construction: wip

---
+++ tests/ProductPageTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Copyright (c) 2025 Bruno Meilick
+ * All rights reserved.
+ *
+ * This file is part of Kirby Kart and is proprietary software.
+ * Unauthorized copying, modification, or distribution is prohibited.
+ */
+it('has a blueprint from PHP', function () {
+    expect(\Kirby\Data\Yaml::encode(ProductPage::phpBlueprint()))->toMatchSnapshot();
+});
