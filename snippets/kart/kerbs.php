35d9c20207c702df1cf629a91f32eb628f426076
Bruno Meilick <b@bnomei.com>
2025-04-17 20:27:48 +0100

    :construction: wip

--- snippets/kart/kerbs.php
+++ snippets/kart/kerbs.php
@@ -1,21 +1,41 @@
 <?php
 
+/**
+ * Copyright (c) 2025 Bruno Meilick
+ * All rights reserved.
+ *
+ * This file is part of Kirby Kart and is proprietary software.
+ * Unauthorized copying, modification, or distribution is prohibited.
+ *
+ * Inertia Adapter is based on code by
+ * Copyright (c) 2020 Jon Gacnik
+ * https://github.com/monoeq/kirby-inertia
+ * (MIT license)
+ */
+
 use Kirby\Cms\Response;
+use Kirby\Toolkit\A;
 
 $page ??= kirby()->site()->page();
 $template ??= $page->intendedTemplate();
 $props ??= $page->toKerbs();
+if ($props instanceof \Kirby\Content\Field) {
+    $props = [];
+}
 $request = kirby()->request();
-
-// TODO: partials
-// TODO: (maybe) shared global props
 
 $inertia = array_filter([
     'component' => ucfirst($template->name()),
     'props' => array_map(fn($value) => $value instanceof Closure ? $value() : $value, $props + kart()->option('kerbs.shared')),
     'url' => $request->url()->toString(),
-    'version' => kart()->option('kerbs.version'), // TODO: read manifest file if it exists
+    'version' => kart()->option('kerbs.version'),
 ]);
+
+// only return partial props when requested
+$only = array_filter(explode(',', $request->header('X-Inertia-Partial-Data') ?? ''));
+$inertia['props'] = ($only && $request->header('X-Inertia-Partial-Component') === $inertia['component'])
+    ? A::get($inertia['props'], $only)
+    : $inertia['props'];
 
 // return json when in inertia mode
 if ($request->method() === 'GET' && $request->header('X-Inertia')) {
@@ -28,13 +48,15 @@ if ($request->method() === 'GET' && $request->header('X-Inertia')) {
 
 // otherwise render the app
 ?><!DOCTYPE html>
-<html lang="en">
+<html lang="<?= kirby()->language()?->code() ?? 'en' ?>">
 <head>
-    <title><?= site()->title() ?></title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= page()->isHomePage() ? site()->title() : page()->title().' | '.site()->title() ?></title>
 </head>
 <body>
     <!-- Kart Kerbs -->
-    <div id="<?= $id ?? 'app' ?>" data-page='<?= json_encode($inertia) ?>'></div>
+    <div id="<?= $appId ?? 'app' ?>" data-page='<?= json_encode($inertia) ?>'></div>
     <script src="<?= kirby()->urls()->media() ?>/plugins/bnomei/kart/kerbs.iife.js"></script>
 </body>
 </html>
