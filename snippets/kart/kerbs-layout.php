cf36f1654a630e91b32cbc27bca410ed06f40a40
Bruno Meilick <b@bnomei.com>
2025-04-18 11:30:08 +0100

    :construction: wip

--- snippets/kart/kerbs-layout.php
+++ snippets/kart/kerbs-layout.php
@@ -4,6 +4,17 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= page()->isHomePage() ? site()->title() : page()->title().' | '.site()->title() ?></title>
+    <style>
+        @font-face {
+            font-family: "Geist";
+            font-weight: 100 900;
+            font-display: block;
+            src: local("Geist"), url('/assets/fonts/Geist[wght].woff2') format("woff2");
+        }
+        body {
+            font-family: "Geist", sans-serif;
+        }
+    </style>
 </head>
 <body>
 <?= $slot ?>
